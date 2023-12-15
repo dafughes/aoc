1ab70f71d4ac7567e620073081271d66a13e898c
Samuel Hughes <samuel.hughes@aalto.fi>
2023-12-15 14:07:11 +0200

    day 13 refactor

--- day13.py
+++ day13.py
@@ -1,14 +1,10 @@
 from utils import *
 
-rows = []
-cols = []
+input = []
 
 for p in (arr.split('\n') for arr in read_input(13).split('\n\n')):
-    w = len(p[0])
-    h = len(p)
-
-    r = [0] * h
-    c = [0] * w
+    r = [0] * len(p)
+    c = [0] * len(p[0])
 
     for y, line in enumerate(p):
         for x, ch in enumerate(line):
@@ -16,21 +12,19 @@ for p in (arr.split('\n') for arr in read_input(13).split('\n\n')):
                 r[y] |= 2 ** x
                 c[x] |= 2 ** y
 
-    rows.append(r)
-    cols.append(c)
+    input.append((r, c))
 
 def solution(p, task=1):
     for i in range(1, len(p)):
-        reflection = zip(p[:i][::-1], p[i:])
-        if task == 1 and all(a == b for a, b in reflection):
+        if task == 1 and all(a == b for a, b in zip(p[:i][::-1], p[i:])):
             return i
         elif task == 2 and sum((a ^ b).bit_count() for a, b in zip(p[:i][::-1], p[i:])) == 1:
             return i
 
     return 0 
 
-task1 = sum(100 * solution(r) + solution(c) for r, c in zip(rows, cols))
-task2 = sum(100 * solution(r, 2) + solution(c, 2) for r, c in zip(rows, cols))
+task1 = sum(100 * solution(r) + solution(c) for r, c in input)
+task2 = sum(100 * solution(r, 2) + solution(c, 2) for r, c in input)
     
 print(task1)
 print(task2)
